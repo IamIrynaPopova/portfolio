e7afcc824e1b481de2fb9380719d8caefca6a4e5
Iryna Popova <popovaira969@gmail.com>
2024-02-04 23:49:35 +0200

    add font

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,6 @@ module.exports = {
   },
   module: {
     rules: [
-    
       {
         test: /\.(sa|sc|c)ss$/,
         use: [
@@ -72,6 +71,10 @@ module.exports = {
             },
           },
         },
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: "asset/resource",
       },
     ],
