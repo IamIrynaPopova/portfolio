fb7f6a7b0c4bec86b53320ba67326f8987c0841d
Iryna Popova <popovaira969@gmail.com>
2024-02-05 22:51:50 +0200

    update webpack and create scss

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import example from "./example";
-import human from "./assets/human.jpg";
 import sprite from "./assets/sprite.svg";
-import css from "./styles/main.css";
+import css from "./styles/main.scss";
 
 
 
