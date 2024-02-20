90f0928d68665000c87fb320a3549f942adfdbe5
Iryna Popova <popovaira969@gmail.com>
2024-02-20 18:42:11 +0200

    added projects

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import human from "./assets/human.jpg";
 import portfolio from "./assets/portfolio.jpg";
 import news from "./assets/news.jpg";
 import foodSTA from "./assets/foodSTA.jpg";
+import SoYummy from "./assets/SoYummy.png";
+import zerno from "./assets/zerno.jpg";
 import sprite from "./assets/sprite.svg";
 import sprite2 from "./assets/sprite2.svg";
 import css from "./styles/main.scss";
