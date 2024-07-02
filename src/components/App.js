65678d709b87ed8dcb87e5e6cc4f4bb2934c0ceb
Iryna Popova <popovaira969@gmail.com>
2024-07-02 17:20:16 +0300

    fix

--- src/components/App.js
+++ src/components/App.js
@@ -38,10 +38,6 @@ const App = () => {
 
   useEffect(() => {
     handleResize();
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
   }, [screenWidth]);
 
   const onClickMobileOpen = () => {
